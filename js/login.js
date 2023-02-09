222a7c4fae3a40ff12f8965caf869bf4872522c0
iesteed <94650446+iesteed@users.noreply.github.com>
2023-02-08 20:21:34 -0500

    Update login.js
    Adds if statement to validate email and password
    
    (Needs DB integration)

--- js/login.js
+++ js/login.js
@@ -1,11 +1,14 @@
 document.getElementById("loginButton").addEventListener("click", loginFunc);
 
 function loginFunc() {
-    // Attempts to log in, redirecting to the dashboard with valid email and password.
-    // TODO:
-    // Check email and password in DB
-    // Redirect to dashboard page
-    // Alert user on incorrect login info
     let email = document.getElementById("form2Example17").value
     let password = document.getElementById("form2Example27").value
+
+    // Needs DB integration
+    if (email && password) {
+        window.location.href = " /dashboard.html";
+    }
+    else {
+        alert("Invalid login. Please try again.")
+    }
 }
