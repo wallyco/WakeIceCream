da07c264a15c227ec668e635ad069431b24f75e4
Joshua Pierce <piercejosh96@gmail.com>
2023-02-27 20:03:10 -0500

    Cleaning up the code

--- php/query_for_login.php
+++ php/query_for_login.php
@@ -7,16 +7,25 @@ $connect = new mysqli('db', 'wakeicecream_user', 'password', 'WakeIceCreamDB');
 //     print_r("Connected \n");
 // }
 
-$table_name = "Staff";
+$table_name_staff = "Staff";
+$table_name_role = "Role";
 
-$query = "SELECT * FROM $table_name WHERE Email='$login_id'";
+$query = "SELECT * FROM $table_name_staff  WHERE Email='$login_id'";
 
 $response = mysqli_query($connect, $query); 
 $responseval = mysqli_fetch_assoc($response);
 $querypass = $responseval['Password'];
+$queryroleID = $responseval['Role_ID'];
+
+$query = "SELECT * FROM $table_name_role  WHERE Role_ID='$queryroleID'";
+$response = mysqli_query($connect, $query); 
+$responseval = mysqli_fetch_assoc($response);
+$queryAuthLevel = $responseval['Auth_Level'];
+
 
 if($querypass == $password){
     echo json_encode(1);
+    echo json_encode((int)$queryAuthLevel);
     //pass
 }else{
     echo json_encode(0);
