4725df90c9c88815e25189d7f078a084be9332d2
Joshua Pierce <piercejosh96@gmail.com>
2023-04-15 08:58:28 -0400

    Database updated

--- database/WakeIceCreamDB.sql
+++ database/WakeIceCreamDB.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: db:3306
--- Generation Time: Mar 11, 2023 at 04:38 PM
+-- Generation Time: Apr 15, 2023 at 12:54 PM
 -- Server version: 8.0.32
 -- PHP Version: 8.1.15
 
@@ -39,7 +39,9 @@ CREATE TABLE `Category` (
 
 INSERT INTO `Category` (`Category_ID`, `Category_Name`, `Description`) VALUES
 (1, 'Ice_cream', 'A soft frozen food make with sweetened and flavored milk fat'),
-(2, 'Topping ', 'Something put in the top of ice cream such as hot fudge, chocolate syrup');
+(2, 'Topping ', 'Something put in the top of ice cream such as hot fudge, chocolate syrup'),
+(3, 'Waffle Cone', 'Joy Waffle Classic Regular Cone '),
+(4, 'Ice Cream Cup ', 'Fabri-Kal 9oz Clear Cup ');
 
 -- --------------------------------------------------------
 
@@ -64,8 +66,14 @@ CREATE TABLE `Product` (
 --
 
 INSERT INTO `Product` (`Product_ID`, `Product_Name`, `Description`, `Product_Unit`, `Product_Price`, `Product_Quantity`, `Product_Status`, `Supplier_ID`, `Category_ID`) VALUES
-(1, 'Chocolate Ice Cream', 'Ice cream with natural chocolate flavoring', 'pint', 90.00, 3, 'Active ', 1, 1),
-(2, 'Mango Topping', 'Topping made from the mango fruit', 'ounce', 25.50, 60, 'Active ', 4, 2);
+(1, 'Mango Mix ', 'Dole Soft Serve Mango Mix ', 'lbs ', 8.00, 4, 'Using ', 3, 1),
+(2, 'Cookies and Frozen Yogurt Ice Cream ', 'Honey Hill Low Fat ', 'gallons ', 7.00, 3, 'Using ', 5, 1),
+(3, 'Toppers Rainbow Sprinkles', 'Gluten Free', 'lbs', 4.00, 6, 'Using', 6, 2),
+(4, 'Semi Sweet Mini Baking Chips ', 'Hershey \'s chips ', 'lbs', 2.50, 10, 'Using ', 4, 2),
+(5, 'Waffle Cone ', 'Large Cone ', 'package ', 1.00, 300, 'Using ', 6, 3),
+(6, 'Mint Chocolate Ice Cream ', 'Non Dairy Mint Chocolate ', 'lbs ', 6.50, 6, 'Using ', 2, 1),
+(7, 'Vanilla Syrup ', 'JHS Vanilla Syrup ', 'gallons ', 2.00, 2, 'Not use', 7, 2),
+(8, 'Plastic Ice cream Cup ', 'Fabri-Kal 9oz ', 'package ', 0.80, 500, 'Using ', 6, 4);
 
 -- --------------------------------------------------------
 
@@ -138,7 +146,10 @@ INSERT INTO `Supplier` (`Supplier_ID`, `Name`, `Address`, `Phone`, `Fax`, `Email
 (1, 'Cheasapeake ', 'Baltimore, MD ', '410-675-6674', '410-675-6670', 'cheasapeake@gmail.com', 'Ice cream cone, ice cream syrup'),
 (2, 'Dutt and Wagner', 'Abingdon, VA', '800-688-2110', '800-688-2112', 'duttwagner@gmail.com', 'Baked sweet goods, soft serve ice cream'),
 (3, 'Nestle_USA', 'Arlington, VA', '703-682-4500', '703-682-4501', 'nestle_usa@hotmail.com', 'Chocolate products.'),
-(4, 'Vista Corporate', 'Englewood, CO', '800-880-9911', '800-880-9922', 'nestle_usa@hotmail.com', 'Ice cream, candies, snacks ');
+(4, 'Vista Corporate', 'Englewood, CO', '800-880-9911', '800-880-9922', 'nestle_usa@hotmail.com', 'Ice cream, candies, snacks '),
+(5, 'Good Time Ice Cream Distributor ', 'Raleigh, NC ', '919-876-3333', '919-876-3330', 'goodtime@gmail.com', 'Base ice cream distribution company that supply ice cream & frozen desset in the Triangle area '),
+(6, 'Regional Distributors Inc', 'Rochester, NY ', '585-485-3300', '585-485-3301', 'customer_service@redist.com ', 'frozen yogurt, disposable cups, bowls, straws, napkins'),
+(7, 'King\'s Cupboard ', 'Red Lodge, MT ', '800-962-6550', '800-962-6551', 'kingcup_service21@hotmail.com', 'Hot, sweet, caramel , organic chcolate, sweeteners and ice-cream topping');
 
 --
 -- Indexes for dumped tables
@@ -185,7 +196,7 @@ ALTER TABLE `Supplier`
 -- AUTO_INCREMENT for table `Category`
 --
 ALTER TABLE `Category`
-  MODIFY `Category_ID` bigint UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `Category_ID` bigint UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `Product`
@@ -203,7 +214,7 @@ ALTER TABLE `Staff`
 -- AUTO_INCREMENT for table `Supplier`
 --
 ALTER TABLE `Supplier`
-  MODIFY `Supplier_ID` bigint UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `Supplier_ID` bigint UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- Constraints for dumped tables
